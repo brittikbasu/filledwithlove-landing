55702089e0295cb51374bbd648a5a7a864bf6373
Brittik basu <brittikbasu@gmail.com>
2018-11-30 22:43:54 +0530

    add animation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React, { Component } from "react";
 import "./App.css";
-import logo from './assets/logo.png';
-import girlheart from './assets/girlheart.png';
-
+import girlheart from "./assets/girlheart.png";
+import { Spring, config } from "react-spring";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <div class="logo">
-          {/* <img
-            src={logo}
-            alt="filled with love - a design agency for startups"
-          /> */}
-          <h1>Filled With 💜</h1>
-          <p>A design agency for startups</p>
-        </div>
+        <Spring
+          config={config.gentle}
+          from={{ opacity: 0, marginTop: "0vh" }}
+          to={{ opacity: 1, marginTop: "15vh" }}
+        >
+          {props => (
+            <div class="logo" style={props}>
+              <h1>Filled With 💜</h1>
+              <p>A design agency for startups</p>
+            </div>
+          )}
+        </Spring>
 
-        <div class="girlheart">
-          <img src={girlheart} />
-        </div>
+        <Spring from={{ opacity: 0 }} to={{ opacity: 1 }}>
+          {props => (
+            <div class="girlheart" style={props}>
+              <img src={girlheart} />
+            </div>
+          )}
+        </Spring>
       </div>
     );
   }
